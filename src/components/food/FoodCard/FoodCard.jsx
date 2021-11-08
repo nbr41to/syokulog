cca7de95bb6365bcb6481a89c8cb07bd99010fd2
nbr41to <nbr.41to@gmail.com>
2021-11-08 22:07:45 +0900

    画像の追加

--- src/components/food/FoodCard/FoodCard.jsx
+++ src/components/food/FoodCard/FoodCard.jsx
@@ -3,6 +3,7 @@ import styles from './index.module.scss';
 export const FoodCard = () => {
   return (
     <div className={styles.wrapper}>
+      <img src="https://picsum.photos/80" alt="" />
       <div className={styles.title}>Food title</div>
       <div className={styles.atDate}>2021/11/03 18:45</div>
     </div>
