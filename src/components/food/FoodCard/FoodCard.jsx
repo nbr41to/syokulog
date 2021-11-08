20298d26c2d6654baa6ec607df176ce09e3a9bb3
nbr41to <nbr.41to@gmail.com>
2021-11-09 00:00:58 +0900

    /foods/postsの追加

--- src/components/food/FoodCard/FoodCard.jsx
+++ src/components/food/FoodCard/FoodCard.jsx
@@ -1,11 +1,22 @@
 import styles from './index.module.scss';
 
-export const FoodCard = ({ title }) => {
+export const FoodCard = ({ title, sendAt, imageUrl, ingredients, memo }) => {
   return (
-    <div className={styles.wrapper}>
-      <img src="https://picsum.photos/80" alt="" />
-      <div className={styles.title}>{title}</div>
-      <div className={styles.atDate}>2021/11/03 18:45</div>
+    <div className={styles.card}>
+      <div className={styles.card__image_wrapper}>
+        <img src={imageUrl} alt="料理の画像" />
+      </div>
+      <div>
+        <div className={styles.title}>{title}</div>
+        <div>
+          食材:
+          {ingredients.map((ingredient, index) => (
+            <span key={index}>[{ingredient}]</span>
+          ))}
+        </div>
+        <div>{memo}</div>
+        <div className={styles.atDate}>{sendAt}</div>
+      </div>
     </div>
   );
 };
--- src/components/pages/FoodPosts/index.jsx
+++ src/components/pages/FoodPosts/index.jsx
@@ -1,14 +1,40 @@
 import { FoodCard } from 'src/components/food/FoodCard/FoodCard';
 import styles from './index.module.scss';
 
+const data = [
+  {
+    id: 'a123',
+    title: 'にしんそば',
+    sendAt: '2020/01/01',
+    imageUrl: 'https://picsum.photos/80',
+    ingredients: ['イワシ', 'そば', 'ごま油'],
+    memo: 'にしんが売ってなかった',
+  },
+  {
+    id: 'a124',
+    title: 'ハンバーグ',
+    sendAt: '2020/01/01',
+    imageUrl: 'https://picsum.photos/80',
+    ingredients: ['ひき肉（牛肉）', '玉ねぎ'],
+  },
+  {
+    id: 'a125',
+    title: 'ラーメン',
+    sendAt: '2020/01/01',
+    imageUrl: 'https://picsum.photos/80',
+    ingredients: ['ひき肉（牛肉）', '玉ねぎ'],
+    memo: '駅前の家系ラーメン屋',
+  },
+];
+
 const FoodPostsPage = () => {
   return (
-    <div className={styles.posts}>
+    <div className={styles.page}>
       <h1>ここは投稿一覧</h1>
       <div>
-        <FoodCard title="にしんそば" />
-        <FoodCard title="ハンバーグ" />
-        <FoodCard title="ラーメン" />
+        {data.map((post) => (
+          <FoodCard key={post.id} {...post} />
+        ))}
       </div>
     </div>
   );
