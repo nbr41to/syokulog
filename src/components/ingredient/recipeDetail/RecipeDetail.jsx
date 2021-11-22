aee3496074e2c21ad560fdeea7fed7f25fa66caa
Chisato Kato <i.like.nuko@gmail.com>
2021-11-22 12:42:08 +0900

    料理詳細ページの作成

--- src/components/ingredient/recipeDetail/RecipeDetail.jsx
+++ src/components/ingredient/recipeDetail/RecipeDetail.jsx
@@ -6,11 +6,44 @@ export const RecipeDetail = ({ recipeData }) => {
       {recipeData?.map((data, index) => (
         <div key={index}>
           <div className={styles.recipe_title}>
-            <img src="" alt="" />
-
-            <span>{/* icon */}</span>
-            <p className={styles.title}>{data.title}</p>
-            <p className={styles.user_name}>{data.username}さん</p>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/1.svg`}
+              alt="Logo"
+              className={styles.recipe_image}
+            />
+            <div className={styles.title_container}>
+              <div className={styles.google_logo}>
+                <h1 className={styles.title}>{data.title}</h1>
+                <img
+                  src={`${process.env.PUBLIC_URL}/images/googleのロゴ 1.svg`}
+                  alt="Logo"
+                  className={styles.google_icon}
+                />
+              </div>
+              <div className={styles.user}>
+                <img
+                  src={`${process.env.PUBLIC_URL}/images/icon 1.svg`}
+                  alt="Logo"
+                  className={styles.icon}
+                />
+                <p className={styles.user_name}>{data.username}さん</p>
+              </div>
+              <img
+                src={`${process.env.PUBLIC_URL}/images/like.svg`}
+                alt="Logo"
+                className={styles.icon}
+              />
+              <img
+                src={`${process.env.PUBLIC_URL}/images/comment.svg`}
+                alt="Logo"
+                className={styles.icon}
+              />
+              <img
+                src={`${process.env.PUBLIC_URL}/images/download.svg`}
+                alt="Logo"
+                className={styles.icon}
+              />
+            </div>
           </div>
           <div className={styles.recipe}>
             <div className={styles.material}>
