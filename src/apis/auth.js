e45b2edde141c46dffbca238c5e723a4e2946cdf
kameda <shunmel.09@gmail.com>
2021-11-28 13:28:06 +0900

    Authenticationの実装 【ログインページ】 ・Googleログイン実装 ・emailとpasswordでログイン実装 【サインイン】 ・emailとpasswordで新規登録

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,15 +1,62 @@
-import { getAuth, signInWithPopup, signOut } from 'firebase/auth';
-import { app } from './firebase';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+import { app } from './config';
 import { GoogleAuthProvider } from 'firebase/auth';
+import { setUserData } from './post';
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
 /**
+ * パスワードとemailでアカウント登録
+ */
+export const register = async (name, email, password) => {
+  try {
+    const result = await createUserWithEmailAndPassword(auth, email, password);
+    const user = result.user;
+    if (user) {
+      const uid = user.uid;
+      const userData = {
+        uid: uid,
+        email: email,
+        username: name,
+      };
+      setUserData(uid, userData);
+    }
+    return result;
+  } catch (error) {
+    alert(error);
+  }
+};
+
+/**
+ * Eメールとパスワードでログイン
+ * @param email
+ * @param password
+ */
+export const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+/**
  * Googleでログイン
  */
-export const signInGoogle = () => {
-  return signInWithPopup(auth, googleProvider);
+export const signInGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    return result;
+  } catch (error) {
+    alert(error);
+  }
 };
 
 /**
--- src/components/pages/Signup/index.jsx
+++ src/components/pages/Signup/index.jsx
@@ -1,27 +1,62 @@
-import styles from './index.module.scss';
+import { useRef } from 'react';
 import { useHistory } from 'react-router-dom';
+import styles from './index.module.scss';
 import defaultIcon from 'src/assets/svg/icon 1.svg';
+import { register } from '../../../apis/auth';
 const SignupPage = () => {
+  // input value
+  const name = useRef('');
+  const email = useRef('');
+  const password = useRef('');
+
+  // Router
   const history = useHistory();
+
+  // handler
+  const handleRegistBtnClick = async () => {
+    const result = await register(
+      name.current,
+      email.current,
+      password.current,
+    );
+    if (result?.user) {
+      history.push('/');
+    }
+  };
 
   const handleLoginLinkClick = () => {
     history.push('/login');
   };
 
-  const handleRegistBtnClick = () => {
-    history.push('/');
-  };
-
+  // jsx
   return (
     <div className={styles.root}>
       <div className={styles.container}>
-        <icon />
         <img src={defaultIcon} alt="Icon" className={styles.icon} />
         <button className={styles.iconRegistLink}>写真を登録</button>
         <div className={styles.inputContainer}>
-          <input placeholder="Name" className={styles.topInput} />
-          <input placeholder="Email" className={styles.middleInput} />
-          <input placeholder="Password" className={styles.bottomInput} />
+          <input
+            placeholder="Name"
+            className={styles.topInput}
+            onChange={(e) => {
+              name.current = e.target.value;
+            }}
+          />
+          <input
+            placeholder="Email"
+            className={styles.middleInput}
+            onChange={(e) => {
+              email.current = e.target.value;
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className={styles.bottomInput}
+            onChange={(e) => {
+              password.current = e.target.value;
+            }}
+          />
         </div>
         <button onClick={handleRegistBtnClick} className={styles.registButton}>
           登録
