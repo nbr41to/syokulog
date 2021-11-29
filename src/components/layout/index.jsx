eb8c6ee8228f43f545181cb8ff06130ac7d8a20e
Knob <59198478+nbr41to@users.noreply.github.com>
2021-11-29 23:34:05 +0900

    headerとfooterを追加

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,18 +1,13 @@
+import { Footer } from './Footer';
+import { Header } from './Header';
 import styles from './index.module.scss';
 
 export const Layout = ({ children }) => {
   return (
     <div className={styles.wrapper}>
-      <header className={styles.header}>
-        <div className={styles.header__text}>食ログ</div>
-        <div className={styles.header__subtext}>- syokulog -</div>
-      </header>
+      <Header className={styles.header} />
       <main className={styles.main}>{children}</main>
-      <footer className={styles.footer}>
-        <div className={styles.footer__text}>
-          © 2021 progLab All rights reserved.
-        </div>
-      </footer>
+      <Footer className={styles.footer} />
     </div>
   );
 };
