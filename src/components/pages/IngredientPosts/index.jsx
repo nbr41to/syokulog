6ac102de53c5da32bf2cdb847af22460518e0f8e
Knob <59198478+nbr41to@users.noreply.github.com>
2021-12-10 00:13:46 +0900

    階層の整理

--- src/components/pages/IngredientPosts/index.jsx
+++ src/components/pages/IngredientPosts/index.jsx
@@ -1,26 +1,10 @@
-import { useEffect, useState } from 'react';
-import { getIngredients } from 'src/apis/post';
+import { IngredientPosts } from 'src/components/Ingredients/IngredientPosts';
 import styles from './index.module.scss';
-import { PostCard } from './PostCard';
 
 const IngredientPostsPage = () => {
-  const [ingredients, setIngredients] = useState([]);
-
-  useEffect(() => {
-    void (async () => {
-      const response = await getIngredients();
-      setIngredients(response);
-    })();
-  }, []);
-
   return (
     <div className={styles.posts}>
-      <h1>食材投稿一覧</h1>
-      <div>
-        {ingredients.map((ingredient) => (
-          <PostCard key={ingredient.id} ingredient={ingredient} />
-        ))}
-      </div>
+      <IngredientPosts />
     </div>
   );
 };
