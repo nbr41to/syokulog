3408fc5f478874c6f1469bdcd462990538de931e
藤島太智 <4ts0t0i7c@gmail.com>
2021-12-12 22:13:55 +0900

    指摘修正

--- src/components/pages/MyProfile/index.jsx
+++ src/components/pages/MyProfile/index.jsx
@@ -3,23 +3,26 @@ import noIconImage from '../../../assets/svg/icon 1.svg';
 import styles from './index.module.scss';
 
 const MyProfilePage = () => {
-  // return <div>マイページ</div>;
   return (
     <div className={styles.pageWrap}>
       <div className={styles.titleButton}>
         <div className={styles.title}>プロフィール</div>
-        <input className={styles.editButton} type="" value="編集" />
+        <input className={styles.editButton} type="submit" value="編集" />
       </div>
       <div className={styles.profileBox}>
         <div className={styles.iconBox}>
           <img src={noIconImage} alt="" />
         </div>
         <div className={styles.nameIntroductionBox}>
-          <input className={styles.name} type="text" placeholder="名前" />
+          <input
+            className={styles.name}
+            name=""
+            type="text"
+            placeholder="名前"
+          />
           <textarea
             className={styles.introduction}
             name=""
-            id=""
             placeholder="自己紹介"
             cols="30"
             rows="10"
