f47da1ce7112ea026de12c470ae3a14a45c573ed
axtx4869 <shota.furuno@claves.co.jp>
2022-01-27 23:52:59 +0900

    APIキーをenv.localから読み込むようにした

--- src/components/pages/Weather/index.jsx
+++ src/components/pages/Weather/index.jsx
@@ -5,13 +5,14 @@ import currentLocationImage from '../../../assets/svg/current-location.svg';
 
 //TODO: 降水確率を別のAPIから取得する
 //TODO: 最低・最高気温の前日比を取得する(昨日の天気を取得する)
+//TODO: 昨日の最低気温・最高気温はhistoricalのAPIを叩く
 //TODO: 一週間の天気を取得する
 //TODO: 3時間ごとの天気を取得する
 
 const WeatherPage = () => {
   const [position, setPosition] = useState(undefined);
-  const [weatherForToday, setWeatherForToday] = useState(undefined);
-
+  const [weatherDataForToday, setWeatherDataForToday] = useState(undefined);
+  const API_KEY = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY;
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (currentPosition) => {
@@ -39,18 +40,49 @@ const WeatherPage = () => {
 
   useEffect(async () => {
     if (position) {
-      const weatherApi = axios.create({
-        baseURL: `https://api.openweathermap.org/data/2.5/onecall?lat=${position.latitude}&lon=${position.longitude}&exclude=current,minutely,hourly,alerts&units=metric&appid=ffd4cbb59c5533b7bc189dd3f4f21005`,
+      const apiWeatherDataForToday = axios.create({
+        baseURL: `https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&exclude=minutely,hourly,daily,alerts&units=metric&lang=ja&appid=${API_KEY}`,
       });
-      const res = await weatherApi.get();
-      console.log(res.data);
-      setWeatherForToday({
-        main: res.data.daily[0].weather[0].main,
-        minTemperature: res.data.daily[0].temp.min,
-        maxTemperature: res.data.daily[0].temp.max,
+      const responseDataForToday = await apiWeatherDataForToday.get();
+      console.log(responseDataForToday.data);
+      setWeatherDataForToday({
+        main: responseDataForToday.data.weather[0].main,
+        minTemp: responseDataForToday.data.main.temp_min,
+        maxTemp: responseDataForToday.data.main.temp_max,
       });
     }
   }, [position]);
+
+  useEffect(async () => {
+    if (position && weatherDataForToday) {
+      const date = new Date();
+      date.setDate(date.getDate() - 1);
+      const unixTimestampPerMillisecond = date.getTime();
+      const unixTImestampPerSecond = Math.floor(
+        unixTimestampPerMillisecond / 1000,
+      );
+      const apiWeatherDataForYesterday = axios.create({
+        baseURL: `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${position.latitude}&lon=${position.longitude}&dt=${unixTImestampPerSecond}&units=metric&appid=${API_KEY}`,
+      });
+      const responseDataForYesterday = await apiWeatherDataForYesterday.get();
+      const hourlyTemps = [];
+      responseDataForYesterday.data.hourly.map((data) => {
+        hourlyTemps.push(data.temp);
+      });
+      const maxTempForYesterday = Math.max(...hourlyTemps);
+      const minTempForYesterday = Math.min(...hourlyTemps);
+      const changeOfMaxTempFromYesterday =
+        weatherDataForToday.maxTemp - maxTempForYesterday;
+      const changeOfMinTempFromYesterday =
+        weatherDataForToday.minTemp - minTempForYesterday;
+      setWeatherDataForToday({
+        ...weatherDataForToday,
+        changeOfMaxTempFromYesterday: changeOfMaxTempFromYesterday,
+        changeOfMinTempFromYesterday: changeOfMinTempFromYesterday,
+      });
+    }
+  }, [weatherDataForToday]);
+
   return (
     <>
       {position ? (
@@ -61,11 +93,13 @@ const WeatherPage = () => {
             <span>2021年12月1日</span>
           </div>
           <div>
-            {weatherForToday && (
+            {weatherDataForToday && (
               <div>
-                <h3>{weatherForToday.main}</h3>
-                <h3>{weatherForToday.minTemperature}&deg;C</h3>
-                <h3>{weatherForToday.maxTemperature}&deg;C</h3>
+                <h3>{weatherDataForToday.main}</h3>
+                <h3>{weatherDataForToday.minTemp}&deg;C</h3>
+                <h3>{weatherDataForToday.maxTemp}&deg;C</h3>
+                <h3>{weatherDataForToday.changeOfMaxTempFromYesterday}</h3>
+                <h3>{weatherDataForToday.changeOfMinTempFromYesterday}</h3>
               </div>
             )}
           </div>
