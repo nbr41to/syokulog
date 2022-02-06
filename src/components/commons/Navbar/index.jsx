d9f2b4085ffda18cf764cedc87cd74b4afc9fe01
shun <shunmel.09@gmail.com>
2022-02-07 07:56:25 +0900

    ハンバーガーメニュー修正
    全遷移先追加。ハンバーガーメニューのアニメーション速度修正。スクロールできるよう修正

--- src/components/commons/Navbar/index.jsx
+++ src/components/commons/Navbar/index.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './index.module.scss';
 import weather from '../../../assets/svg/weather.svg';
 import search from '../../../assets/svg/search.svg';
 import home from '../../../assets/svg/home.svg';
@@ -9,9 +7,25 @@ import add from '../../../assets/svg/add.svg';
 // ナビゲーションバーアイテム
 const menuItems = [
   { title: 'ホーム', imgPath: home, to: '/' },
-  { title: '検索', imgPath: search, to: 'foods/search' },
-  { title: '投稿', imgPath: add, to: '/foods/posts' },
-  { title: '天気', imgPath: weather, to: '/weather' },
+  { title: '投稿一覧（料理）', imgPath: search, to: '/foods/posts' },
+  { title: '投稿一覧（食材）', imgPath: add, to: '/ingredients/posts' },
+  { title: '投稿詳細（料理）', imgPath: weather, to: '/foods/posts/a123' },
+  {
+    title: '新規投稿（料理）',
+    imgPath: weather,
+    to: '/ingredients/posts/a123',
+  },
+  { title: '新規投稿（食材）', imgPath: weather, to: '/ingredients/new' },
+  { title: '検索（料理）', imgPath: weather, to: '/foods/search' },
+  { title: '検索（食材）', imgPath: weather, to: '/ingredients/search' },
+  { title: '詳細（料理）', imgPath: weather, to: '/foods/a123' },
+  { title: '詳細（食材）', imgPath: weather, to: '/ingredients/a123' },
+  { title: '天気の確認', imgPath: weather, to: '/weather' },
+  { title: 'ログイン', imgPath: weather, to: '/login' },
+  { title: 'アカウント登録', imgPath: weather, to: '/signup' },
+  { title: 'マイページ', imgPath: weather, to: '/my/profile' },
+  { title: '自分が記録した料理一覧', imgPath: weather, to: '/my/foods' },
+  { title: '自分が記録した食材一覧', imgPath: weather, to: '/my/ingredients' },
 ];
 
 export const Navbar = ({ closeMenu }) => (
@@ -36,21 +50,34 @@ export const Navbar = ({ closeMenu }) => (
         `}
     </style>
     {/* 背景色：グレー */}
-    <div className={styles.navBgcolor} onClick={closeMenu}>
+    <div
+      style={{
+        width: '374px',
+        height: '784px',
+        position: 'absolute',
+        animation: 'name 0.7s forwards',
+      }}
+      onClick={closeMenu}
+    >
       {/* ナビゲーションバー */}
       <div
         style={{
           borderRight: '1px solid rgba(0, 0, 0, 0.2)',
           width: '224px',
-          height: '918px',
+          height: '784px',
+          overflow: 'auto',
           backgroundColor: 'white',
           position: 'absolute',
-          animation: 'toLeft 1s forwards',
+          animation: 'toLeft 0.7s forwards',
           transform: 'translateX(100%)',
         }}
       >
         {/* ナビゲーションバータイトル */}
-        <div style={{ textAlign: 'center' }}>
+        <div
+          style={{
+            textAlign: 'center',
+          }}
+        >
           <p>メニュー</p>
         </div>
         {/* ナビゲーションバーアイテム */}
