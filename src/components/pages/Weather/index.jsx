7707b2825475f4d30bd52d7c116f10e54fad01b4
axtx4869 <shota.furuno@claves.co.jp>
2022-02-19 14:17:05 +0900

    コンポーネント内部でコンポーネントを定義しないようにした

--- src/components/pages/Weather/index.jsx
+++ src/components/pages/Weather/index.jsx
@@ -1,138 +1,140 @@
 import { useState } from 'react';
 import styles from './index.module.scss';
 import CurrentLoactionImage from '../../../assets/svg/current-location.svg';
-import SunnyImage from '../../../assets/svg/sunny.svg';
 import Loading from '../../../assets/gif/loading.gif';
 import { useWeatherData } from '../../../apis/hooks/useWeatherData';
 import { useWeatherIcon } from '../../../apis/hooks/useWeatherIcon';
 import dayjs from 'dayjs';
 import ja from 'dayjs/locale/ja';
 
-const WeatherPage = () => {
-  const { currentWeatherData, dailyWeatherData } = useWeatherData();
-  dayjs.locale(ja);
-  const now = dayjs();
+const TodaySection = ({ currentWeatherData, hourlyWeatherData }) => {
+  const [disabled, setDisabled] = useState(true);
+  return (
+    <section className={styles.mainWeatherWrapper}>
+      <div className={styles.buttonWrapper}>
+        <button
+          type="button"
+          disabled={disabled}
+          onClick={() => setDisabled(!disabled)}
+        >
+          今日
+        </button>
+        <button
+          type="button"
+          disabled={!disabled}
+          onClick={() => setDisabled(!disabled)}
+        >
+          3時間
+        </button>
+      </div>
+      {disabled ? (
+        <Current currentWeatherData={currentWeatherData} />
+      ) : (
+        <EveryThreeHours hourlyWeatherData={hourlyWeatherData} />
+      )}
+    </section>
+  );
+};
 
-  const A = () => {
-    const [disabled, setDisabled] = useState(true);
-    return (
-      <section className={styles.mainWeatherWrapper}>
-        <div className={styles.buttonWrapper}>
-          <button
-            type="button"
-            disabled={disabled}
-            onClick={() => setDisabled(!disabled)}
-          >
-            今日
-          </button>
-          <button
-            type="button"
-            disabled={!disabled}
-            onClick={() => setDisabled(!disabled)}
-          >
-            3時間
-          </button>
+const Current = ({ currentWeatherData }) => {
+  return (
+    <div className={styles.todayWrapper}>
+      <div className={styles.weatherArea}>
+        <div className={styles.weatherImageWrapper}>
+          <img
+            src={useWeatherIcon(currentWeatherData.weather)}
+            alt="weather image"
+          />
         </div>
-        {disabled ? <Today /> : <EveryThreeHours />}
-      </section>
-    );
-  };
-
-  const Today = () => {
-    return (
-      <div className={styles.todayWrapper}>
-        <div className={styles.weatherArea}>
-          <div className={styles.weatherImageWrapper}>
-            <img
-              src={useWeatherIcon(currentWeatherData.weather)}
-              alt="weather image"
-            />
-          </div>
-          <div>
-            <span>{currentWeatherData.weather}</span>
-          </div>
-        </div>
-        <div className={styles.temperatureArea}>
-          <div>
-            <div>
-              <span>最高</span>
-              <b>{Math.round(currentWeatherData.maxTemp)}&deg;C</b>
-              <span>[0]</span>
-            </div>
-            <div></div>
-          </div>
-          <div>
-            <div>
-              <span>最低</span>
-              <b>{Math.round(currentWeatherData.minTemp)}&deg;C</b>
-              <span>[0]</span>
-            </div>
-            <div></div>
-          </div>
-          <div>
-            <span>降水確率15&#037;</span>
-          </div>
+        <div>
+          <span>{currentWeatherData.weatherDescription}</span>
         </div>
       </div>
-    );
-  };
-
-  const EveryThreeHours = () => {
-    const hours = [0, 3, 6, 9, 12, 15, 18, 21];
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>時間</th>
-            <th>天気</th>
-            <th>気温</th>
-            <th>降水</th>
-          </tr>
-        </thead>
-        <tbody>
-          {hours.map((hour) => {
-            return (
-              <tr key={hour}>
-                <td>{hour}</td>
-                <td>
-                  <img src={SunnyImage} alt="weather logo" />
-                </td>
-                <td>10&deg;C</td>
-                <td>0mm</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    );
-  };
-
-  const OneWeek = () => {
-    return (
-      <section className={styles.weeklyWeatherWrapper}>
-        <h3>一週間の天気</h3>
+      <div className={styles.temperatureArea}>
         <div>
-          {dailyWeatherData.map((data, index) => {
-            return (
-              <div className={styles.weeklyWeatherItem} key={index}>
-                <time dateTime={data.date.format('YYYY-MM-DD')}>
-                  {data.date.month() + 1}月{data.date.date()}日（
-                  {data.date.format('dd')}）
-                </time>
-                <img src={useWeatherIcon(data.weather)} alt="weather image" />
-                <span>20%</span>
-                <span>
-                  {Math.round(data.maxTemp)} / {Math.round(data.minTemp)}
-                </span>
-              </div>
-            );
-          })}
+          <div>
+            <span>最高</span>
+            <b>{Math.round(currentWeatherData.maxTemp)}&deg;C</b>
+            <span>[0]</span>
+          </div>
+          <div></div>
         </div>
-      </section>
-    );
-  };
+        <div>
+          <div>
+            <span>最低</span>
+            <b>{Math.round(currentWeatherData.minTemp)}&deg;C</b>
+            <span>[0]</span>
+          </div>
+          <div></div>
+        </div>
+        <div>
+          <span>降水確率15&#037;</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-  return currentWeatherData && dailyWeatherData ? (
+const EveryThreeHours = ({ hourlyWeatherData }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>時間</th>
+          <th>天気</th>
+          <th>気温</th>
+          <th>降水</th>
+        </tr>
+      </thead>
+      <tbody>
+        {hourlyWeatherData.map((data, index) => {
+          return (
+            <tr key={index}>
+              <td>{index * 3}</td>
+              <td>
+                <img src={useWeatherIcon(data.weather)} alt="weather icon" />
+              </td>
+              <td>{Math.round(data.temp)}&deg;C</td>
+              <td>{Math.round(data.rainVolume)}mm</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+const DailySection = ({ dailyWeatherData }) => {
+  return (
+    <section className={styles.weeklyWeatherWrapper}>
+      <h3>一週間の天気</h3>
+      <div>
+        {dailyWeatherData.map((data, index) => {
+          return (
+            <div className={styles.weeklyWeatherItem} key={index}>
+              <time dateTime={data.date.format('YYYY-MM-DD')}>
+                {data.date.month() + 1}月{data.date.date()}日（
+                {data.date.format('dd')}）
+              </time>
+              <img src={useWeatherIcon(data.weather)} alt="weather image" />
+              <span>20%</span>
+              <span>
+                {Math.round(data.maxTemp)} / {Math.round(data.minTemp)}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+const WeatherPage = () => {
+  const { currentWeatherData, dailyWeatherData, hourlyWeatherData } =
+    useWeatherData();
+  dayjs.locale(ja);
+  const now = dayjs();
+  return currentWeatherData && dailyWeatherData && hourlyWeatherData ? (
     <>
       <section className={styles.currentInfoWrapper}>
         <div className={styles.currentLocation}>
@@ -145,8 +147,11 @@ const WeatherPage = () => {
           </time>
         </div>
       </section>
-      <A />
-      <OneWeek />
+      <TodaySection
+        currentWeatherData={currentWeatherData}
+        hourlyWeatherData={hourlyWeatherData}
+      />
+      <DailySection dailyWeatherData={dailyWeatherData} />
     </>
   ) : (
     <div className={styles.loadingWrapper}>
