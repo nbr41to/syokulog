dad2f8d607db0b1f2a7033b47d0ab27e3ac30903
shun <shunmel.09@gmail.com>
2022-02-28 08:37:53 +0900

    未ログインの場合はハンバーガーメニューを隠す

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,13 +1,30 @@
 import styles from './index.module.scss';
 import { ReactComponent as HamburgerIcon } from 'src/assets/svg/three-line.svg';
 import { ReactComponent as AccountIcon } from 'src/assets/svg/parson.svg';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from 'src/apis/config';
+import { useState } from 'react';
 
 export const Header = ({ openMenu }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  onAuthStateChanged(auth, (user) => {
+    setCurrentUser(user);
+  });
   return (
-    <header className={styles.header}>
-      <HamburgerIcon onClick={openMenu} className={styles.hamburger} />
-      <div className={styles.header__text}>☆食ログ☆</div>
-      <AccountIcon className={styles.account} />
-    </header>
+    <>
+      {currentUser === null ? (
+        <header className={styles.header}>
+          <div className={styles.hamburger} />
+          <div className={styles.header__text}>☆食ログ☆</div>
+          <AccountIcon className={styles.account} />
+        </header>
+      ) : (
+        <header className={styles.header}>
+          <HamburgerIcon onClick={openMenu} className={styles.hamburger} />
+          <div className={styles.header__text}>☆食ログ☆</div>
+          <AccountIcon className={styles.account} />
+        </header>
+      )}
+    </>
   );
 };
